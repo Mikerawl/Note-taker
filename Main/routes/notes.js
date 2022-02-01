629f9998f8f08a670cb3d157fcc1df170363265a
Michael Rawlings <rawlingsm99@gmail.com>
2022-01-31 22:06:40 -0600

    made some minor changes today

--- Main/routes/notes.js
+++ Main/routes/notes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 
 // Renders the HTML index page.
-router.get('/', (req, res) => {res.render("index")})
-
-
+router.get('/', (req, res) => {
+    res.render('notes')
+})
 
 module.exports=router
+
--- Main/server.js
+++ Main/server.js
@@ -1,16 +1,24 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = 3001;
 
-app.set("view engine", ".ejs");
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 
 app.use(express.static("public"));
 
 app.use("/", require("./routes/index"));
 
+app.use("/notes", require("./routes/notes"));
+
   app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
 
+  
 
 
 
