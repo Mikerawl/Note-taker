45c0e644c3f466832672c33ffb6815cc63f6f492
Michael Rawlings <rawlingsm99@gmail.com>
2022-02-01 19:46:31 -0600

    added notes to th e second html page by incorporating handlebars.

--- Main/routes/notes.js
+++ Main/routes/notes.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const fs = require("fs");
+const _ = require("lodash");
+const {v4:uuidv4} = require("uuid");
+
+const {
+    readFromFile, readAndAppend, writeToFile
+} = require("../helpers/fsUtils")
 
 
 // Renders the HTML index page.
 router.get('/', (req, res) => {
-    res.render('notes')
+    let data = fs.readFileSync("./db/notes.json");
+    let notes = JSON.parse(data);
+    console.log(notes);
+
+    res.render('notes', {notes})
 })
 
 module.exports=router
--- Main/server.js
+++ Main/server.js
@@ -1,20 +1,22 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const {engine} = require('express-handlebars');
+const path = require ("path");
 
 const app = express();
 const PORT = 3001;
 
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
+app.engine('.hbs', engine({extname:'.hbs'}));
+app.set('view engine', '.hbs');
+// app.set('views', path.join(__dirname,(views)))
 
 app.use(express.static("public"));
 
-app.use("/", require("./routes/index"));
+const indexRouter = require('./routes/index')
+const notesRouter = require('./routes/notes')
 
-app.use("/notes", require("./routes/notes"));
+app.use("/", indexRouter);
+app.use("/notes", notesRouter);
 
   app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
 
