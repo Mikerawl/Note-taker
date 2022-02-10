8d9895fad39e8c08df91e9786d9d7bf28d01359b
Michael Rawlings <rawlingsm99@gmail.com>
2022-02-09 20:25:43 -0600

    functionality is complere.  Next stop will be appearance.

--- Main/routes/notes.js
+++ Main/routes/notes.js
@@ -1,3 +1,5 @@
+const { json } = require('body-parser');
+const e = require('express');
 const express = require('express');
 const router = express.Router();
 const fs = require("fs");
@@ -13,10 +15,49 @@ const {
 router.get('/', (req, res) => {
     let data = fs.readFileSync("./db/notes.json");
     let notes = JSON.parse(data);
-    console.log(notes);
-
+ 
     res.render('notes', {notes})
 })
 
+router.post('/', async (req, res) => {
+   const {title, note} = req.body;
+
+   if (req.body) {
+       const newNote = {title, note, id:uuidv4()}
+    await readAndAppend(newNote, "./db/notes.json")
+    res.redirect("notes")
+    }else {
+        res.err("error in add")
+    }    
+})
+
+
+router.get('/:id', (req, res) => {
+    fs.readFile("./db/notes.json", "utf8", function(err, data) {
+     let notes = JSON.parse(data); 
+           
+           let note = _.find(notes, function(o){
+            return o.id === req.params.id});
+            console.log(note);
+            res.render('note', {note});
+        })  
+    })
+
+router.delete('/delete/:id', (req, res) => {
+    let data = fs.readFileSync("./db/notes.json");
+    let notes = JSON.parse(data);
+    let index = _.findIndex(notes, function(d) {
+        return d.id === req.params.id
+    })
+    if (index > -1) {
+        notes.splice(index, 1)
+    }
+    writeToFile("./db/notes.json", notes)
+
+   res.render("notes", {notes}) 
+})
+
+
 module.exports=router
+
 
