cf9e7dd38f0ef2f7135d998afb8ab4b9a6784018
Michael Rawlings <rawlingsm99@gmail.com>
2022-02-10 14:12:18 -0600

    made changes to help deploy live link

--- Main/server.js
+++ Main/server.js
@@ -7,7 +7,7 @@ const methodOverride = require ("method-override");
 
 const app = express();
 // Assigns the port
-const PORT = process.env.port || 3002;
+const PORT = process.env.port || 3001;
 
 // Allowsexpress to get data from forms
 app.use(bodyParser.urlencoded({extended:true}));
@@ -41,7 +41,7 @@ app.use("/", indexRouter);
 app.use("/notes", notesRouter);
 
 // Start the server on the port
-  app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
+  app.listen(PORT, () => console.log(`Example app ${PORT}`));
 
 
 
